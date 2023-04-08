a366180c6b8e2af2a645d78d7924b38b72c04f3b
Raf <rafael090105@gmail.com>
2023-04-08 23:29:22 +0700

    register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,84 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{asset('style/loginstyle.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <form action="/login" method="POST">
         @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div id="container">
+            <div id="top-wallpaper">
+                <div id="top-content">
+                    <img src="assets/My Events.png" alt="myevents">
+                    <a href="home.html" id="home"><b>Home</b></a>
+                </div>
+                <div id="bottom-content">
+                    <h3>Take your pick</h3>
+                    <p>from the hottest events in town<br>and book your tickets today!</p>
+                </div>
+            </div>
+    
+           <div id="login-page">
+                <div id="top">
+                    <img src="assets/My Events.png" alt="">
+                    <h2>Login</h2>
+                    <p>Not a member yet? <a href="/register" id="register-link">Register</a></p>
+                </div>
+                <form action="">
+                    <div class="form-content">
+                        <label for="email">Email</label>
+                        <input type="text">
+                    </div>
+                    <div class="form-content">
+                        <div id="password">
+                            <label for="password">Password</label>
+                            <a href="">Forgot password</a>
+                        </div>
+                        <div id="input-box">
+                            <input type="password" id="input-password">
+                            <i id="button-visible"><img src="assets/Unhidden.png" alt="eye" id="eye" width="41" height="30"></i>
+                        </div>
+                    </div>
+            
+                    <div id="checkbox">
+                        <input type="checkbox">
+                        <p>Remember me</p>
+                    </div>
+    
+                    <button type="submit" id="login">Login</button>
+    
+                    <div id="line">
+                        <p><span>or</span></p>
+                    </div>
+    
+                    <div class="login-media">
+                        <img src="assets/Facebook.png" alt="">
+                        <a href="">Login with Facebook</a>
+                    </div>
+    
+                    <div class="login-media">
+                        <img src="assets/Apple.png" alt="">
+                        <a href="">Login with Apple</a>
+                    </div>
+    
+                    <div class="login-media">
+                        <img src="assets/Google.png" alt="">
+                        <a href="">Login with Google</a>
+                    </div>
+    
+                </form>
+           </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
+    
     </form>
-</x-guest-layout>
+    
+    <script src="script/script.js"></script>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,55 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link rel="stylesheet" href="{{asset('style/registerstyle.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <div id="container">
+        <div id="top-wallpaper">
+            <div id="top-content">
+                <img src="assets/My Events.png" alt="myevents">
+                <a href="/viewEvent" id="home"><b>Home</b></a>
+            </div>
+            <div id="bottom-content">
+                <h3>Take your pick</h3>
+                <p>from the hottest events in town<br>and book your tickets today!</p>
+            </div>
+        </div>
+
+       <div id="register-page">
+            <div id="top">
+                <img src="assets/My Events.png" alt="">
+                <h2>Register</h2>
+                <p>Already have an account? <a href="/login" id="login-link">Login</a></p>
+            </div>
+
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Name -->
-        <div>
+        <div class="form-content">
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
+        <div class="form-content">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="form-content">
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
@@ -29,7 +61,7 @@
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
+        <div class="form-content">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
@@ -39,14 +71,27 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="form-content">
+            <x-input-label for="phone" :value="__('phone')" />
+            <x-text-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required  />
+            <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+        </div>
+
+        <div id="checkbox">
+            <input type="checkbox">
+            <p>I have read and agree to the <a href="">Terms and Agreement</a></p>
+        </div>
+
+        {{-- <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
-            </a>
+            </a> --}}
 
-            <x-primary-button class="ml-4">
+            <button type="submit" id="register">
                 {{ __('Register') }}
-            </x-primary-button>
+            </button>
         </div>
     </form>
-</x-guest-layout>
+</body>
+</html>
+    
